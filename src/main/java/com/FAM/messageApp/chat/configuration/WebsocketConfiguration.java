f9fd583afab8c927f59458bdc56f6bb8c5d55aab
Elghobashy <ahmadsamir694@gmail.com>
2023-05-04 19:54:25 +0300

    fixed cors error

--- src/main/java/com/FAM/messageApp/chat/configuration/WebsocketConfiguration.java
+++ src/main/java/com/FAM/messageApp/chat/configuration/WebsocketConfiguration.java
@@ -2,7 +2,6 @@ package com.FAM.messageApp.chat.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -12,7 +11,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebsocketConfiguration implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat").withSockJS();
+        registry.addEndpoint("/chat").setAllowedOriginPatterns("*").withSockJS();
 
     }
 
