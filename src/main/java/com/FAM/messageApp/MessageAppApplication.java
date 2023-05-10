f5798c48b746472b255946472639153dc5de4db7
Zeyad ElSaeed <ziadelnagar380@gmail.com>
2023-05-10 19:37:40 +0300

    logging
    to use logging, add annotation @Slf4j, then use "log" instance to add log

--- src/main/java/com/FAM/messageApp/MessageAppApplication.java
+++ src/main/java/com/FAM/messageApp/MessageAppApplication.java
@@ -2,6 +2,7 @@ package com.FAM.messageApp;
 
 import com.FAM.messageApp.dao.ChatRepository;
 import com.FAM.messageApp.model.Chat;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,7 @@ import java.util.Arrays;
 
 @SpringBootApplication
 @CrossOrigin(originPatterns = "*", allowedHeaders = "*")
+@Slf4j
 public class MessageAppApplication {
 
 	public static void main(String[] args) {
